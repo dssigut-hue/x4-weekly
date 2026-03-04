236cc8f49e6e081bfb1a24626bcd9f1394433582
David S <davids@SAX25-SIGDAV.local>
2026-03-04 21:43:56 +0100

    fix async meeting page

--- src/app/weekly/[year]/[cw]/meeting/page.tsx
+++ src/app/weekly/[year]/[cw]/meeting/page.tsx
@@ -4,7 +4,7 @@ import MeetingMode from "@/components/MeetingMode";
 
 type Props = { params: { year: string; cw: string } };
 
-export default function MeetingPage({ params }: Props) {
+export default async function MeetingPage({ params }: Props) {
   const year = parseInt(params.year, 10);
   const cw = parseInt(params.cw, 10);
   const weekly = await upsertWeekly(year, cw);
